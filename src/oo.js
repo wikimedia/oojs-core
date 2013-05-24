e83d3f308737f1a937141ec4bc4244dc167c7753
Trevor Parscal <trevorparscal@gmail.com>
2013-05-25 00:17:36 +0200

    Initial import of utility functions and EventEmitter class

--- src/oo.js
+++ src/oo.js
@@ -1,22 +1,294 @@
+/*!
+ * Object Oriented JavaScript.
+ *
+ * @copyright 2011-2013 VisualEditor Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
+
 ( function ( global ) {
 
-	var OO = {
+'use strict';
 
-		/**
-		 * @param {Mixed} o
-		 * @return {boolean}
-		 */
-		isAwesome: function ( o ) {
-			return o !== undefined;
-		}
+var
+	/**
+	 * Namespace for all classes, static methods and static properties.
+	 * @class
+	 * @singleton
+	 */
+	oo = {},
+	hasOwn = Object.prototype.hasOwnProperty;
 
-	};
+/* Class Methods */
 
-	/*jshint node:true */
-	if ( typeof module !== 'undefined' && module.exports ) {
-		module.exports = OO;
-	} else {
-		global.OO = OO;
+/**
+ * Utility for common usage of #createObject for inheriting from one
+ * prototype to another.
+ *
+ * Beware: This redefines the prototype, call before setting your prototypes.
+ * Beware: This redefines the prototype, can only be called once on a function.
+ *  If called multiple times on the same function, the previous prototype is lost.
+ *  This is how prototypal inheritance works, it can only be one straight chain
+ *  (just like classical inheritance in PHP for example). If you need to work with
+ *  multiple constructors consider storing an instance of the other constructor in a
+ *  property instead, or perhaps use a mixin (see oo.mixinClass).
+ *
+ *     function Foo() {}
+ *     Foo.prototype.jump = function () {};
+ *
+ *     function FooBar() {}
+ *     oo.inheritClass( FooBar, Foo );
+ *     FooBar.prop.feet = 2;
+ *     FooBar.prototype.walk = function () {};
+ *
+ *     function FooBarQuux() {}
+ *     OO.inheritClass( FooBarQuux, FooBar );
+ *     FooBarQuux.prototype.jump = function () {};
+ *
+ *     FooBarQuux.prop.feet === 2;
+ *     var fb = new FooBar();
+ *     fb.jump();
+ *     fb.walk();
+ *     fb instanceof Foo && fb instanceof FooBar && fb instanceof FooBarQuux;
+ *
+ * @method
+ * @param {Function} targetFn
+ * @param {Function} originFn
+ * @throws {Error} If target already inherits from origin
+ */
+oo.inheritClass = function ( targetFn, originFn ) {
+	if ( targetFn.prototype instanceof originFn ) {
+		throw new Error( 'Target already inherits from origin' );
 	}
+
+	// Doesn't really require ES5 (jshint/jshint#74@github)
+	/*jshint es5: true */
+	var targetConstructor = targetFn.prototype.constructor;
+
+	targetFn.prototype = oo.createObject( originFn.prototype );
+
+	// Restore constructor property of targetFn
+	targetFn.prototype.constructor = targetConstructor;
+
+	// Extend static properties - always initialize both sides
+	originFn.static = originFn.static || {};
+	targetFn.static = oo.createObject( originFn.static );
+
+	// Copy mixin tracking
+	targetFn.mixins = originFn.mixins ? originFn.mixins.slice( 0 ) : [];
+};
+
+/**
+ * Utility to copy over *own* prototype properties of a mixin.
+ * The 'constructor' (whether implicit or explicit) is not copied over.
+ *
+ * This does not create inheritance to the origin. If inheritance is needed
+ * use oo.inheritClass instead.
+ *
+ * Beware: This can redefine a prototype property, call before setting your prototypes.
+ * Beware: Don't call before oo.inheritClass.
+ *
+ *     function Foo() {}
+ *     function Context() {}
+ *
+ *     // Avoid repeating this code
+ *     function ContextLazyLoad() {}
+ *     ContextLazyLoad.prototype.getContext = function () {
+ *         if ( !this.context ) {
+ *             this.context = new Context();
+ *         }
+ *         return this.context;
+ *     };
+ *
+ *     function FooBar() {}
+ *     OO.inheritClass( FooBar, Foo );
+ *     OO.mixinClass( FooBar, ContextLazyLoad );
+ *
+ * @method
+ * @param {Function} targetFn
+ * @param {Function} originFn
+ */
+oo.mixinClass = function ( targetFn, originFn ) {
+	var key;
+
+	// Copy prototype properties
+	for ( key in originFn.prototype ) {
+		if ( key !== 'constructor' && hasOwn.call( originFn.prototype, key ) ) {
+			targetFn.prototype[key] = originFn.prototype[key];
+		}
+	}
+
+	// Copy static properties - always initialize both sides
+	targetFn.static = targetFn.static || {};
+	if ( originFn.static ) {
+		for ( key in originFn.static ) {
+			if ( hasOwn.call( originFn.static, key ) ) {
+				targetFn.static[key] = originFn.static[key];
+			}
+		}
+	} else {
+		originFn.static = {};
+	}
+};
+
+/* Object Methods */
+
+/**
+ * Create an object that inherits from another object.
+ *
+ * @method
+ * @until ES5: Object.create
+ * @param {Object} origin Object to inherit from
+ * @return {Object} Empty object that inherits from origin
+ */
+oo.createObject = Object.create || function ( origin ) {
+	function O() {}
+	O.prototype = origin;
+	var r = new O();
+
+	return r;
+};
+
+/**
+ * Create a new object that is an instance of the same
+ * constructor as the input, inherits from the same object
+ * and contains the same own properties.
+ *
+ * This makes a shallow non-recursive copy of own properties.
+ * To create a recursive copy of plain objects, use #copy.
+ *
+ *     var foo = new Person( mom, dad );
+ *     foo.setAge( 21 );
+ *     var foo2 = OO.cloneObject( foo );
+ *     foo.setAge( 22 );
+ *
+ *     // Then
+ *     foo2 !== foo; // true
+ *     foo2 instanceof Person; // true
+ *     foo2.getAge(); // 21
+ *     foo.getAge(); // 22
+ *
+ * @method
+ * @param {Object} origin
+ * @return {Object} Clone of origin
+ */
+oo.cloneObject = function ( origin ) {
+	var key, r;
+
+	r = oo.createObject( origin.constructor.prototype );
+
+	for ( key in origin ) {
+		if ( hasOwn.call( origin, key ) ) {
+			r[key] = origin[key];
+		}
+	}
+
+	return r;
+};
+
+/**
+ * Gets an array of all property values in an object.
+ *
+ * @method
+ * @param {Object} Object to get values from
+ * @returns {Array} List of object values
+ */
+oo.getObjectValues = function ( obj ) {
+	var key, values;
+
+	if ( obj !== Object( obj ) ) {
+		throw new TypeError( 'Called on non-object' );
+	}
+
+	values = [];
+	for ( key in obj ) {
+		if ( hasOwn.call( obj, key ) ) {
+			values[values.length] = obj[key];
+		}
+	}
+
+	return values;
+};
+
+/**
+ * Recursively compares string and number property between two objects.
+ *
+ * A false result may be caused by property inequality or by properties in one object missing from
+ * the other. An asymmetrical test may also be performed, which checks only that properties in the
+ * first object are present in the second object, but not the inverse.
+ *
+ * @method
+ * @param {Object} a First object to compare
+ * @param {Object} b Second object to compare
+ * @param {boolean} [asymmetrical] Whether to check only that b contains values from a
+ * @returns {boolean} If the objects contain the same values as each other
+ */
+oo.compareObjects = function ( a, b, asymmetrical ) {
+	var aValue, bValue, aType, bType, k;
+	for ( k in a ) {
+		aValue = a[k];
+		bValue = b[k];
+		aType = typeof aValue;
+		bType = typeof bValue;
+		if ( aType !== bType ||
+			( ( aType === 'string' || aType === 'number' ) && aValue !== bValue ) ||
+			( aValue === Object( aValue ) && !oo.compareObjects( aValue, bValue ) ) ) {
+			return false;
+		}
+	}
+	// If the check is not asymmetrical, recursing with the arguments swapped will verify our result
+	return asymmetrical ? true : oo.compareObjects( b, a, true );
+};
+
+/**
+ * Create a plain deep copy of any kind of object.
+ *
+ * Copies are deep, and will either be an object or an array depending on `source`.
+ *
+ * @method
+ * @param {Object} source Object to copy
+ * @param {Function} [callback] Applied to leaf values before they added to the clone
+ * @returns {Object} Copy of source object
+ */
+oo.copy = function ( source, callback ) {
+	var key, sourceValue, sourceType, destination;
+
+	if ( typeof source.clone === 'function' ) {
+		return source.clone();
+	}
+
+	destination = Array.isArray( source ) ? new Array( source.length ) : {};
+
+	for ( key in source ) {
+		sourceValue = source[key];
+		sourceType = typeof sourceValue;
+		if ( Array.isArray( sourceValue ) ) {
+			// Array
+			destination[key] = oo.copy( sourceValue, callback );
+		} else if ( sourceValue && typeof sourceValue.clone === 'function' ) {
+			// Duck type object with custom clone method
+			destination[key] = callback ?
+				callback( sourceValue.clone() ) : sourceValue.clone();
+		} else if ( sourceValue && typeof sourceValue.cloneNode === 'function' ) {
+			// DOM Node
+			destination[key] = callback ?
+				callback( sourceValue.cloneNode( true ) ) : sourceValue.cloneNode( true );
+		} else if ( sourceValue === Object( sourceValue ) && typeof sourceValue !== 'function' ) {
+			// Other objects, except for functions that we cannot clone
+			destination[key] = oo.copy( sourceValue, callback );
+		} else {
+			// Primitive values and functions
+			destination[key] = callback ? callback( sourceValue ) : sourceValue;
+		}
+	}
+
+	return destination;
+};
+
+/*jshint node:true */
+if ( typeof module !== 'undefined' && module.exports ) {
+	module.exports = oo;
+} else {
+	global.OO = oo;
+}
 
 }( this ) );
--- test/oo.test.js
+++ test/oo.test.js
@@ -1,5 +1,444 @@
-QUnit.module( 'oojs' );
+/*!
+ * Object Oriented JavaScript tests.
+ *
+ * @copyright 2011-2013 VisualEditor Team and others; see AUTHORS.txt
+ * @license The MIT License (MIT); see LICENSE.txt
+ */
 
-QUnit.test( 'isAwesome', 1, function ( assert ) {
-	assert.strictEqual( OO.isAwesome( 42 ), true, 'The Answer is awesome.' );
+( function ( oo ) {
+
+QUnit.module( 'oo' );
+
+/* Tests */
+
+QUnit.test( 'createObject', 4, function ( assert ) {
+	var foo, bar, fooKeys, barKeys;
+
+	foo = {
+		a: 'a of foo',
+		b: 'b of foo'
+	};
+
+	bar = oo.createObject( foo );
+
+	// Add an own property, hiding the inherited one.
+	bar.b = 'b of bar';
+
+	// Add an own property, hiding an inherited property
+	// that will be added later
+	bar.c = 'c of bar';
+
+	// Add more properties to the origin object,
+	// should be visible in the inheriting object.
+	foo.c = 'c of foo';
+	foo.d = 'd of foo';
+
+	// Different property that only one of each has
+	foo.foo = true;
+	bar.bar = true;
+
+	assert.deepEqual(
+		foo,
+		{
+			a: 'a of foo',
+			b: 'b of foo',
+			c: 'c of foo',
+			d: 'd of foo',
+			foo: true
+		},
+		'Foo has expected properties'
+	);
+
+	assert.deepEqual(
+		bar,
+		{
+			a: 'a of foo',
+			b: 'b of bar',
+			c: 'c of bar',
+			d: 'd of foo',
+			foo: true,
+			bar: true
+		},
+		'Bar has expected properties'
+	);
+
+	fooKeys = Object.keys( foo );
+	barKeys = Object.keys( bar );
+
+	assert.deepEqual(
+		fooKeys,
+		['a', 'b', 'c', 'd', 'foo'],
+		'Own properties of foo'
+	);
+
+	assert.deepEqual(
+		barKeys,
+		['b', 'c', 'bar'],
+		'Own properties of bar'
+	);
 } );
+
+QUnit.test( 'inheritClass', 18, function ( assert ) {
+	var foo, bar;
+
+	function Foo() {
+		this.constructedFoo = true;
+	}
+
+	Foo.a = 'prop of Foo';
+	Foo.b = 'prop of Foo';
+	Foo.prototype.b = 'proto of Foo';
+	Foo.prototype.c = 'proto of Foo';
+	Foo.prototype.bFn = function () {
+		return 'proto of Foo';
+	};
+	Foo.prototype.cFn = function () {
+		return 'proto of Foo';
+	};
+
+	foo = new Foo();
+
+	function Bar() {
+		this.constructedBar = true;
+	}
+	oo.inheritClass( Bar, Foo );
+
+	assert.deepEqual(
+		Foo.static,
+		{},
+		'A "static" property (empty object) is automatically created if absent'
+	);
+
+	Foo.static.a = 'static of Foo';
+	Foo.static.b = 'static of Foo';
+
+	assert.notStrictEqual( Foo.static, Bar.static, 'Static property is not copied, but inheriting' );
+	assert.equal( Bar.static.a, 'static of Foo', 'Foo.static inherits from Bar.static' );
+
+	Bar.static.b = 'static of Bar';
+
+	assert.equal( Foo.static.b, 'static of Foo', 'Change to Bar.static does not affect Foo.static' );
+
+	Bar.a = 'prop of Bar';
+	Bar.prototype.b = 'proto of Bar';
+	Bar.prototype.bFn = function () {
+		return 'proto of Bar';
+	};
+
+	assert.throws( function () {
+		oo.inheritClass( Bar, Foo );
+	}, 'Throw if target already inherits from source (from an earlier call)' );
+
+	assert.throws( function () {
+		oo.inheritClass( Bar, Object );
+	}, 'Throw if target already inherits from source (naturally, Object)' );
+
+	bar = new Bar();
+
+	assert.strictEqual(
+		Bar.b,
+		undefined,
+		'Constructor properties are not inherited'
+	);
+
+	assert.strictEqual(
+		foo instanceof Foo,
+		true,
+		'foo instance of Foo'
+	);
+	assert.strictEqual(
+		foo instanceof Bar,
+		false,
+		'foo not instance of Bar'
+	);
+
+	assert.strictEqual(
+		bar instanceof Foo,
+		true,
+		'bar instance of Foo'
+	);
+	assert.strictEqual(
+		bar instanceof Bar,
+		true,
+		'bar instance of Bar'
+	);
+
+	assert.equal( bar.constructor, Bar, 'constructor property is restored' );
+	assert.equal( bar.b, 'proto of Bar', 'own methods go first' );
+	assert.equal( bar.bFn(), 'proto of Bar', 'own properties go first' );
+	assert.equal( bar.c, 'proto of Foo', 'prototype properties are inherited' );
+	assert.equal( bar.cFn(), 'proto of Foo', 'prototype methods are inherited' );
+
+	Bar.prototype.dFn = function () {
+		return 'proto of Bar';
+	};
+	Foo.prototype.dFn = function () {
+		return 'proto of Foo';
+	};
+	Foo.prototype.eFn = function () {
+		return 'proto of Foo';
+	};
+
+	assert.equal( bar.dFn(), 'proto of Bar', 'inheritance is live (overwriting an inherited method)' );
+	assert.equal( bar.eFn(), 'proto of Foo', 'inheritance is live (adding a new method deeper in the chain)' );
+} );
+
+QUnit.test( 'mixinClass', 4, function ( assert ) {
+	var quux;
+
+	function Foo() {}
+	Foo.prototype.aFn = function () {
+		return 'proto of Foo';
+	};
+
+	function Bar() {}
+	// oo.inheritClass makes the 'constructor'
+	// property an own property when it restores it.
+	oo.inheritClass( Bar, Foo );
+	Bar.prototype.bFn = function () {
+		return 'mixin of Bar';
+	};
+
+	function Quux() {}
+	oo.mixinClass( Quux, Bar );
+
+	assert.strictEqual(
+		Quux.prototype.aFn,
+		undefined,
+		'mixin inheritance is not copied over'
+	);
+
+	assert.strictEqual(
+		Quux.prototype.constructor,
+		Quux,
+		'constructor property skipped'
+	);
+
+	assert.strictEqual(
+		Quux.prototype.hasOwnProperty( 'bFn' ),
+		true,
+		'mixin properties are now own properties, not inherited'
+	);
+
+	quux = new Quux();
+
+	assert.equal( quux.bFn(), 'mixin of Bar', 'mixin method works as expected' );
+} );
+
+QUnit.test( 'cloneObject', 4, function ( assert ) {
+	var myfoo, myfooClone, expected;
+
+	function Foo( x ) {
+		this.x = x;
+	}
+	Foo.prototype.x = 'default';
+	Foo.prototype.aFn = function () {
+		return 'proto of Foo';
+	};
+
+	myfoo = new Foo( 10 );
+	myfooClone = oo.cloneObject( myfoo );
+
+	assert.notStrictEqual( myfoo, myfooClone, 'clone is not equal when compared by reference' );
+	assert.deepEqual( myfoo, myfooClone, 'clone is equal when recursively compared by value' );
+
+	expected = {
+		x: 10,
+		aFn: 'proto of Foo',
+		constructor: Foo,
+		instanceOf: true,
+		own: {
+			x: true,
+			aFn: false,
+			constructor: false
+		}
+	};
+
+	assert.deepEqual(
+		{
+			x: myfoo.x,
+			aFn: myfoo.aFn(),
+			constructor: myfoo.constructor,
+			instanceOf: myfoo instanceof Foo,
+			own: {
+				x: myfoo.hasOwnProperty( 'x' ),
+				aFn: myfoo.hasOwnProperty( 'aFn' ),
+				constructor: myfoo.hasOwnProperty( 'constructor' )
+			}
+		},
+		expected,
+		'original looks as expected'
+	);
+
+	assert.deepEqual(
+		{
+			x: myfooClone.x,
+			aFn: myfooClone.aFn(),
+			constructor: myfooClone.constructor,
+			instanceOf: myfooClone instanceof Foo,
+			own: {
+				x: myfooClone.hasOwnProperty( 'x' ),
+				aFn: myfooClone.hasOwnProperty( 'aFn' ),
+				constructor: myfoo.hasOwnProperty( 'constructor' )
+			}
+		},
+		expected,
+		'clone looks as expected'
+	);
+
+} );
+
+QUnit.test( 'getObjectValues', 6, function ( assert ) {
+	var tmp;
+
+	assert.deepEqual(
+		oo.getObjectValues( { a: 1, b: 2, c: 3, foo: 'bar' } ),
+		[ 1, 2, 3, 'bar' ],
+		'Simple object with numbers and strings as values'
+	);
+	assert.deepEqual(
+		oo.getObjectValues( [ 1, 2, 3, 'bar' ] ),
+		[ 1, 2, 3, 'bar' ],
+		'Simple array with numbers and strings as values'
+	);
+
+	tmp = function () {
+		this.isTest = true;
+
+		return this;
+	};
+	tmp.a = 'foo';
+	tmp.b = 'bar';
+
+	assert.deepEqual(
+		oo.getObjectValues( tmp ),
+		['foo', 'bar'],
+		'Function with properties'
+	);
+
+	assert.throws(
+		function () {
+			oo.getObjectValues( 'hello' );
+		},
+		TypeError,
+		'Throw exception for non-object (string)'
+	);
+
+	assert.throws(
+		function () {
+			oo.getObjectValues( 123 );
+		},
+		TypeError,
+		'Throw exception for non-object (number)'
+	);
+
+	assert.throws(
+		function () {
+			oo.getObjectValues( null );
+		},
+		TypeError,
+		'Throw exception for non-object (null)'
+	);
+} );
+
+QUnit.test( 'copy', 7, function ( assert ) {
+	var simpleArray = [ 'foo', 3, true, false ],
+		withObj = [ { 'bar': 'baz', 'quux': 3 }, 5, null ],
+		nestedArray = [ [ 'a', 'b' ], [ 1, 3, 4 ] ],
+		sparseArray = [ 'a', undefined, undefined, 'b' ],
+		withSparseArray = [ [ 'a', undefined, undefined, 'b' ] ],
+		withFunction = [ function () { return true; } ],
+		Cloneable = function ( p ) {
+			this.p = p;
+		};
+
+	Cloneable.prototype.clone = function () {
+		return new Cloneable( this.p + '-clone' );
+	};
+
+	assert.deepEqual(
+		oo.copy( simpleArray ),
+		simpleArray,
+		'Simple array'
+	);
+	assert.deepEqual(
+		oo.copy( withObj ),
+		withObj,
+		'Array containing object'
+	);
+	assert.deepEqual(
+		oo.copy( [ new Cloneable( 'bar' ) ] ),
+		[ new Cloneable( 'bar-clone' ) ],
+		'Use the .clone() method if available'
+	);
+	assert.deepEqual(
+		oo.copy( nestedArray ),
+		nestedArray,
+		'Nested array'
+	);
+	assert.deepEqual(
+		oo.copy( sparseArray ),
+		sparseArray,
+		'Sparse array'
+	);
+	assert.deepEqual(
+		oo.copy( withSparseArray ),
+		withSparseArray,
+		'Nested sparse array'
+	);
+	assert.deepEqual(
+		oo.copy( withFunction ),
+		withFunction,
+		'Array containing function'
+	);
+} );
+
+QUnit.test( 'copy', 7, function ( assert ) {
+	var simpleObj = { 'foo': 'bar', 'baz': 3, 'quux': null, 'truth': true, 'falsehood': false },
+		nestedObj = { 'foo': { 'bar': 'baz', 'quux': 3 }, 'whee': 5 },
+		withArray = { 'foo': [ 'a', 'b' ], 'bar': [ 1, 3, 4 ] },
+		withSparseArray = { 'foo': [ 'a', undefined, undefined, 'b' ] },
+		withFunction = { 'func': function () { return true; } },
+		Cloneable = function ( p ) {
+			this.p = p;
+		};
+	Cloneable.prototype.clone = function () { return new Cloneable( this.p + '-clone' ); };
+
+	assert.deepEqual(
+		oo.copy( simpleObj ),
+		simpleObj,
+		'Simple object'
+	);
+	assert.deepEqual(
+		oo.copy( nestedObj ),
+		nestedObj,
+		'Nested object'
+	);
+	assert.deepEqual(
+		oo.copy( new Cloneable( 'foo' ) ),
+		new Cloneable( 'foo-clone' ),
+		'Cloneable object'
+	);
+	assert.deepEqual(
+		oo.copy( { 'foo': new Cloneable( 'bar' ) } ),
+		{ 'foo': new Cloneable( 'bar-clone' ) },
+		'Object containing object'
+	);
+	assert.deepEqual(
+		oo.copy( withArray ),
+		withArray,
+		'Object with array'
+	);
+	assert.deepEqual(
+		oo.copy( withSparseArray ),
+		withSparseArray,
+		'Object with sparse array'
+	);
+	assert.deepEqual(
+		oo.copy( withFunction ),
+		withFunction,
+		'Object with function'
+	);
+} );
+
+}( OO ) );
