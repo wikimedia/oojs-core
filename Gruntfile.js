55feaa36552d7b4683b3e6092d99517e3555b38f
Timo Tijhof <krinklemail@gmail.com>
2014-06-27 00:26:05 +0200

    build: Use grunt-contrib-concat instead of homegrown 'combine'
    Parameters for the task concat (targets with dest/src pairs)
    are compatible.
    
    Change-Id: I987bc01c107933b6012a75cd7252dc4098656639

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,7 @@
 /*jshint node:true */
 module.exports = function ( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+	grunt.loadNpmTasks( 'grunt-contrib-concat' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 	grunt.loadNpmTasks( 'grunt-qunit-istanbul' );
@@ -15,7 +16,10 @@ module.exports = function ( grunt ) {
 		clean: {
 			dist: 'dist/*'
 		},
-		combine: {
+		concat: {
+			options: {
+				banner: grunt.file.read( 'src/banner.txt' )
+			},
 			oojs: {
 				dest: 'dist/oojs.js',
 				src: [
@@ -94,45 +98,7 @@ module.exports = function ( grunt ) {
 		} );
 	} );
 
-	grunt.registerMultiTask( 'combine', function () {
-		var isBad = false,
-			compiled = '',
-			name = this.data.dest,
-			src = this.data.src,
-			version = grunt.config( 'pkg.version' );
-
-		src.forEach( function ( filepath ) {
-			var text = grunt.file.read( filepath );
-
-			// Ensure files use only \n for line endings, not \r\n
-			if ( /\x0d\x0a/.test( text ) ) {
-				grunt.log.error( filepath + ': Incorrect line endings (\\r\\n)' );
-				isBad = true;
-			}
-
-			compiled += text;
-		} );
-
-		if ( isBad ) {
-			return false;
-		}
-
-		// Replace version and date placeholders
-		compiled = compiled
-			.replace( /@VERSION/g, version )
-			.replace( /@DATE/g, new Date() );
-
-		grunt.file.write( name, compiled );
-
-		// Fail task if errors were logged.
-		if ( this.errorCount ) {
-			return false;
-		}
-
-		grunt.log.ok( 'File "' + name + '" created.' );
-	} );
-
-	grunt.registerTask( 'build', [ 'clean', 'combine' ] );
+	grunt.registerTask( 'build', [ 'clean', 'concat' ] );
 	grunt.registerTask( 'test', [ 'git-build', 'build', 'jshint', 'jscs', 'qunit' ] );
 	grunt.registerTask( 'default', 'test' );
 };
