f686515051f628fe8c21d373be518a0368bee163
Timo Tijhof <krinklemail@gmail.com>
2015-08-25 02:16:36 +0200

    build: Update saucelabs.browsers.js documentation
    Per https://gerrit.wikimedia.org/r/#/c/233641/4/tests/saucelabs.browsers.js
    
    Change-Id: Id68cb9813797b5f35db86b267b6e455a3d969d44

--- tests/saucelabs.browsers.js
+++ tests/saucelabs.browsers.js
@@ -3,41 +3,40 @@
 /**
  * Cover a wide range of browsers with (ideally) no more than 6 browsers (2 batches of 3).
  *
- * The below covers:
- * - Latest Chrome/Firefox (generally the same on any platform).
- * - IE 11 (latest).
- * - IE 9 (latest before ES5 support)
- * - IE 6 (oldest IE we support)
- * - Safari 5.1 on Mac (oldest IE we support that Sauce Labs provides)
- *
  * More info: https://saucelabs.com/platforms
  */
 module.exports = {
+	// Latest Chrome
 	slChrome: {
 		base: 'SauceLabs',
 		browserName: 'chrome'
 	},
+	// Latest Firefox
 	slFirefox: {
 		base: 'SauceLabs',
 		browserName: 'firefox'
 	},
+	// Oldest Safari that Sauce Labs provides
 	slSafari6: {
 		base: 'SauceLabs',
 		browserName: 'safari',
 		version: '6'
 	},
+	// Latest IE
 	slIE11: {
 		base: 'SauceLabs',
 		platform: 'Windows 7',
 		browserName: 'internet explorer',
 		version: '11'
 	},
+	// Latest IE before full ES5 support
 	slIE9: {
 		base: 'SauceLabs',
 		platform: 'Windows 7',
 		browserName: 'internet explorer',
 		version: '9'
 	},
+	// Oldest IE we support
 	slIE6: {
 		base: 'SauceLabs',
 		platform: 'Windows XP',
