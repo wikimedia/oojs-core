c49ee10d10ab7237820a2827e492b6a8ac7dae4a
Moriel Schottlender <moriel@gmail.com>
2015-10-01 17:09:01 -0700

    Add EmitterList class
    Upstream oojs-ui's GroupElement into a stripped-down OO.EmitterList that
    handles a group of OO.EventEmitter objects and their events.
    
    Bug: T114707
    Change-Id: Ib94e4e4a49011736b3027239e6836343901a9022

--- tests/node-index.js
+++ tests/node-index.js
@@ -17,6 +17,7 @@ qunit.run( {
 		'./tests/unit/core.test.js',
 		'./tests/unit/util.test.js',
 		'./tests/unit/EventEmitter.test.js',
+		'./tests/unit/EmitterList.test.js',
 		'./tests/unit/Registry.test.js',
 		'./tests/unit/Factory.test.js'
 	]
