c558b81b318e508de69b5feee4a7ad6d7c33df5c
Timo Tijhof <krinklemail@gmail.com>
2019-04-05 18:17:14 +0100

    Factory: Improve unit tests
    * Separate invalid/valid use cases.
    
    * Reduce verbosity in assertion messages,
      describe only the subject or purpose of the assertion.
      Let the expected/actual speak for itelf.
    
    * Remove pointless 'lookup()' isolation test, which wasn't really
      isolated given it also tested register(), which made it the same
      as the story-based unit test for 'registration and lookup'.
    
    Change-Id: I7ccf342a5264e1b0b6e746b1321bbc5149322eee

--- tests/unit/Factory.test.js
+++ tests/unit/Factory.test.js
@@ -1,93 +1,87 @@
-( function ( oo ) {
+( function () {
 
 	QUnit.module( 'Factory' );
 
-	/* Stubs */
-
-	oo.FactoryObjectStub = function OoFactoryObjectStub( a, b, c, d ) {
+	function Foo( a, b, c, d ) {
 		this.a = a;
 		this.b = b;
 		this.c = c;
 		this.d = d;
-	};
+	}
+	OO.initClass( Foo );
+	Foo.static.name = 'my-foo';
 
-	oo.initClass( oo.FactoryObjectStub );
-
-	oo.FactoryObjectStub.static.name = 'factory-object-stub';
-
-	/* Tests */
-
-	QUnit.test( 'register/unregister', function ( assert ) {
-		var factory = new oo.Factory();
+	QUnit.test( 'invalid registration', function ( assert ) {
+		var factory = new OO.Factory();
 
 		assert.throws(
 			function () {
 				factory.register( 'not-a-function' );
 			},
 			Error,
-			'Throws an exception when trying to register a non-function value as a constructor'
+			'register non-function as constructor'
 		);
 		assert.throws(
 			function () {
 				factory.register( function UnnamedExample() {} );
 			},
-			'Constructor without .static.name'
+			'register constructor without static.name'
 		);
-
-		factory.register( oo.FactoryObjectStub );
-		assert.strictEqual( factory.lookup( 'factory-object-stub' ), oo.FactoryObjectStub );
-
 		assert.throws(
 			function () {
 				factory.unregister( 'not-a-function' );
 			},
 			Error,
-			'Throws an exception when trying to unregister a non-function value as a constructor'
+			'unregister non-function value as constructor'
 		);
 		assert.throws(
 			function () {
 				factory.unregister( function UnnamedExample() {} );
 			},
-			'Constructor without .static.name'
+			'register constructor without static.name'
 		);
-
-		factory.unregister( oo.FactoryObjectStub );
-		assert.strictEqual( factory.lookup( 'factory-object-stub' ), undefined );
 	} );
 
-	QUnit.test( 'create', function ( assert ) {
-		var obj,
-			factory = new oo.Factory();
+	QUnit.test( 'registeration and lookup', function ( assert ) {
+		var factory = new OO.Factory();
+
+		factory.register( Foo );
+		assert.strictEqual( factory.lookup( 'my-foo' ), Foo );
+
+		factory.unregister( Foo );
+		assert.strictEqual( factory.lookup( 'my-foo' ), undefined );
+	} );
+
+	QUnit.test( 'invalid creation', function ( assert ) {
+		var factory = new OO.Factory();
 
 		assert.throws(
 			function () {
-				factory.create( 'factory-object-stub', 23, 'foo', { bar: 'baz' } );
+				factory.create( 'my-foo', 23, 'foo', { bar: 'baz' } );
 			},
 			Error,
 			'Throws an exception when trying to create a object of an unregistered type'
 		);
+	} );
 
-		factory.register( oo.FactoryObjectStub );
+	QUnit.test( 'valid creation', function ( assert ) {
+		var obj,
+			factory = new OO.Factory();
 
-		obj = factory.create( 'factory-object-stub', 16, 'foo', { baz: 'quux' }, 5 );
+		factory.register( Foo );
+		obj = factory.create( 'my-foo', 16, 'foo', { baz: 'quux' }, 5 );
+
+		assert.strictEqual(
+			obj instanceof Foo,
+			true,
+			'object inherits constructor prototype'
+		);
 
 		assert.deepEqual(
 			obj,
-			new oo.FactoryObjectStub( 16, 'foo', { baz: 'quux' }, 5 ),
-			'Creates an object of the registered type and passes through arguments'
-		);
-
-		assert.strictEqual(
-			obj instanceof oo.FactoryObjectStub,
-			true,
-			'Creates an object that is an instanceof the registered constructor'
+			new Foo( 16, 'foo', { baz: 'quux' }, 5 ),
+			'constructor function ran'
 		);
 	} );
 
-	QUnit.test( 'lookup', function ( assert ) {
-		var factory = new oo.Factory();
-		factory.register( oo.FactoryObjectStub );
-		assert.strictEqual( factory.lookup( 'factory-object-stub' ), oo.FactoryObjectStub );
-	} );
-
-}( OO ) );
+}() );
